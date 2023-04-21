1922386c51cefbf3df844bd2410134481d58f253
Francisco Roldán <francisco.roldang@live.com>
2023-04-20 21:40:11 -0400

    feat: creacion de las vistas y urls

--- aprendizaje3/grupal3/views.py
+++ aprendizaje3/grupal3/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def usuarios(request):
+    lista_usuarios = User.objects.all()
+    return render(request, 'usuarios.html', {'usuarios': lista_usuarios})
